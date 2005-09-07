e4961108510e80c360be25686e31997ed0a2d5bc
lothrop <lothrop@5ab6fc7a-e072-45e7-acf8-72c2705bde4f>
2005-09-07 14:03:27 +0000

    Made tests work with wide strings.

--- trunk/libodbc++/tests/scroll.cpp
+++ trunk/libodbc++/tests/scroll.cpp
@@ -1,18 +1,18 @@
-/* 
+/*
    This file is part of libodbc++.
-   
+
    Copyright (C) 1999-2000 Manush Dodunekov <manush@stendahls.net>
-   
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
-   
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
-   
+
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.  If not, write to
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
@@ -34,17 +34,19 @@
 #include <odbc++/preparedstatement.h>
 
 #include <iostream>
+#include <memory>
 
 using namespace odbc;
 using namespace std;
 
 #if !defined(ODBCXX_QT)
-# include <strstream>
+# include <sstream>
 #else
 
 # undef ASSERT
 
-ostream& operator<<(ostream& o, const QString& s)
+basic_ostream<ODBCXX_CHAR_TYPE> ostream& operator<<(basic_ostream<ODBCXX_CHAR_TYPE>& o,
+                                                    const QString& s)
 {
   o << ODBCXX_STRING_CSTR(s);
   return o;
@@ -53,17 +55,18 @@ ostream& operator<<(ostream& o, const QString& s)
 
 static int assertionsFailed=0;
 
-#define ASSERT(x)						\
-do {								\
-  if(!(x)) {							\
-    cerr << "Assertion \"" << #x << "\" failed" << endl;	\
-    assertionsFailed++;                                         \
-  }								\
+#define ASSERT(x)                                              \
+do {                                                           \
+  if(!(x)) {                                                   \
+    ODBCXX_CERR << ODBCXX_STRING_CONST("Assertion \"") << #x   \
+                << ODBCXX_STRING_CONST("\" failed") << endl;   \
+    assertionsFailed++;                                        \
+  }                                                            \
 } while(false)
 
-#define NAME_PREFIX "odbcxx_"
+#define NAME_PREFIX ODBCXX_STRING_CONST("odbcxx_")
 
-#define TABLE_NAME NAME_PREFIX "test"
+#define TABLE_NAME NAME_PREFIX ODBCXX_STRING_CONST("test")
 
 #define TABLE_ROWS 1000
 
@@ -77,39 +80,36 @@ static void commit(Connection* con)
 static void createStuff(Connection* con)
 {
   // create our table
-  Statement* stmt=con->createStatement();
+  std::auto_ptr<Statement> stmt=std::auto_ptr<Statement>(con->createStatement());
   stmt->executeUpdate
-    ("create table " TABLE_NAME "("
-     "id integer not null primary key, "
-     "name varchar(40) not null)");
-  
-  cout << "Table " << TABLE_NAME << " created." << endl;
-  
-  delete stmt;
+    (ODBCXX_STRING_CONST("create table ") TABLE_NAME ODBCXX_STRING_CONST("(")
+     ODBCXX_STRING_CONST("id integer not null primary key, ")
+     ODBCXX_STRING_CONST("name varchar(40) not null)"));
+
+  ODBCXX_COUT << ODBCXX_STRING_CONST("Table ") << TABLE_NAME
+              << ODBCXX_STRING_CONST(" created.") << endl;
 }
 
 // Drops the database objects.
 
 static void dropStuff(Connection* con)
 {
-  Statement* stmt=con->createStatement();
+  std::auto_ptr<Statement> stmt=std::auto_ptr<Statement>(con->createStatement());
   try {
-    stmt->executeUpdate("drop table " TABLE_NAME);
-    cout << "Dropped table " << TABLE_NAME << endl;
+    stmt->executeUpdate(ODBCXX_STRING_CONST("drop table ") TABLE_NAME);
+    ODBCXX_COUT << ODBCXX_STRING_CONST("Dropped table ") << TABLE_NAME << endl;
   } catch(SQLException& e) {
   }
-  
-  delete stmt;
 }
 
-static ODBCXX_STRING makeName(int n)
+static const ODBCXX_STRING makeName(int n)
 {
 #if !defined(ODBCXX_QT)
-  strstream ss;
-  ss << "This is row number " << n;
-  return string(ss.str(),ss.pcount());
+  basic_ostringstream<ODBCXX_CHAR_TYPE> ss;
+  ss << ODBCXX_STRING_CONST("This is row number ") << n;
+  return ss.str();
 #else
-  QString s("This is row number ");
+  QString s(ODBCXX_STRING_CONST("This is row number "));
   s+=QString::number(n);
   return s;
 #endif
@@ -119,25 +119,27 @@ static ODBCXX_STRING makeName(int n)
 static void populate(Connection* con)
 {
   {
-    PreparedStatement* pstmt=con->prepareStatement
-      ("insert into " TABLE_NAME " (id,name) values(?,?)");
+    std::auto_ptr<PreparedStatement> pstmt
+      =std::auto_ptr<PreparedStatement>(con->prepareStatement
+      (ODBCXX_STRING_CONST("insert into ") TABLE_NAME
+       ODBCXX_STRING_CONST(" (id,name) values(?,?)")));
     for(int i=0; i<TABLE_ROWS; i++) {
       pstmt->setInt(1,i);
       pstmt->setString(2,makeName(i));
       pstmt->executeUpdate();
     }
-    delete pstmt;
     commit(con);
-    cout << "Inserted " << TABLE_ROWS << " rows." << endl;
+    ODBCXX_COUT << ODBCXX_STRING_CONST("Inserted ")
+                << TABLE_ROWS << ODBCXX_STRING_CONST(" rows.") << endl;
   }
 }
 
 
 static void compare(Connection* con)
 {
-  // decide whether we should use a scroll insensitive 
+  // decide whether we should use a scroll insensitive
   // or a scroll sensitive cursor
-  
+
   int rstype;
   int rsconc;
   DatabaseMetaData* md=con->getMetaData();
@@ -147,8 +149,9 @@ static void compare(Connection* con)
   } else if(md->supportsResultSetType(ResultSet::TYPE_SCROLL_SENSITIVE)) {
     rstype=ResultSet::TYPE_SCROLL_SENSITIVE;
   } else {
-    cout << "Skipping compare, data source does not support scrollable cursors" 
-	 << endl;
+    ODBCXX_COUT << ODBCXX_STRING_CONST("Skipping compare, data source does ")
+                   ODBCXX_STRING_CONST("not support scrollable cursors")
+                << endl;
     return;
   }
 
@@ -159,17 +162,17 @@ static void compare(Connection* con)
   } else {
     rsconc=ResultSet::CONCUR_UPDATABLE;
   }
-  
-  Statement* stmt=con->createStatement
-    (rstype,rsconc);
-  ResultSet* rs=stmt->executeQuery
-    ("select id,name from " TABLE_NAME);
+
+  std::auto_ptr<Statement> stmt=std::auto_ptr<Statement>(con->createStatement
+    (rstype,rsconc));
+  std::auto_ptr<ResultSet> rs=std::auto_ptr<ResultSet>(stmt->executeQuery
+    (ODBCXX_STRING_CONST("select id,name from ") TABLE_NAME));
 
   ASSERT(rs->isBeforeFirst());
   ASSERT(rs->first());
   ASSERT(!rs->isBeforeFirst());
   ASSERT(rs->isFirst());
-  
+
   ASSERT(rs->last());
   ASSERT(rs->isLast());
   ASSERT(!rs->isAfterLast());
@@ -178,12 +181,12 @@ static void compare(Connection* con)
 
   ASSERT(rs->previous());
   ASSERT(rs->isLast());
-  
-  
-  cout << "Positioned on the last row (" 
-       << rs->getRow() << ")" << endl;
+
+
+  ODBCXX_COUT << ODBCXX_STRING_CONST("Positioned on the last row (")
+              << rs->getRow() << ODBCXX_STRING_CONST(")") << endl;
   int i=TABLE_ROWS;
-  
+
   do {
     i--;
     ODBCXX_STRING name(makeName(i));
@@ -192,10 +195,9 @@ static void compare(Connection* con)
   } while(rs->previous());
   ASSERT(i==0);
   ASSERT(rs->isBeforeFirst());
-  cout << TABLE_ROWS << " rows checked with expected values." << endl;
-
-  delete stmt; //will kill rs
-    
+  ODBCXX_COUT << TABLE_ROWS
+              << ODBCXX_STRING_CONST(" rows checked with expected values.")
+              << endl;
 }
 
 
@@ -206,21 +208,39 @@ int main(int argc, char** argv)
 {
   if(argc!=2 && argc!=4) {
     cerr << "Usage: " << argv[0] << " connect-string" << endl
-	       << "or     " << argv[0] << " dsn username password" << endl;
+         << "or     " << argv[0] << " dsn username password" << endl;
     return 0;
   }
   try {
-    Connection* con;
-    if(argc==2) {
-      cout << "Connecting to " << argv[1] << "..." << flush;
-      con=DriverManager::getConnection(argv[1]);
-    } else {
-      cout << "Connecting to dsn=" << argv[1]
-	   << ", uid=" << argv[2] 
-	   << ", pwd=" << argv[3] << "..." << flush;
-      con=DriverManager::getConnection(argv[1],argv[2],argv[3]);
+    std::vector<ODBCXX_STRING> vargv(argc-1);
+    const size_t MAX_CHARS = 256;
+    for(int i=1;i<argc;++i)
+    {
+      ODBCXX_STRING& arg=vargv[i-1];
+#if defined(ODBCXX_UNICODE)
+      wchar_t buffer[MAX_CHARS];
+      size_t len=mbstowcs(buffer,argv[i],MAX_CHARS);
+      if(0<len&&MAX_CHARS>len)
+      {
+         arg=buffer;
+      }
+#else
+      arg=argv[i];
+#endif
     }
-    cout << " done." << endl;
+    std::auto_ptr<Connection> con;
+    if(argc==2) {
+      ODBCXX_COUT << ODBCXX_STRING_CONST("Connecting to ") << vargv[0]
+                  << ODBCXX_STRING_CONST("...") << flush;
+      con=std::auto_ptr<Connection>(DriverManager::getConnection(vargv[0]));
+    } else {
+      ODBCXX_COUT << ODBCXX_STRING_CONST("Connecting to dsn=") << vargv[0]
+                  << ODBCXX_STRING_CONST(", uid=") << vargv[1]
+                  << ODBCXX_STRING_CONST(", pwd=") << vargv[2]
+                  << ODBCXX_STRING_CONST("...") << flush;
+      con=std::auto_ptr<Connection>(DriverManager::getConnection(vargv[0],vargv[1],vargv[2]));
+    }
+    ODBCXX_COUT << ODBCXX_STRING_CONST(" done.") << endl;
 
     // we don't want autocommit
     if(con->getMetaData()->supportsTransactions()) {
@@ -229,24 +249,24 @@ int main(int argc, char** argv)
 //      con->setTraceFile("/tmp/fisk");
 //      con->setTrace(true);
 
-    dropStuff(con);
-    createStuff(con);
-    
-    populate(con);
-    compare(con);
-    commit(con);
-    
-    dropStuff(con);
+    dropStuff(con.get());
+    createStuff(con.get());
+
+    populate(con.get());
+    compare(con.get());
+    commit(con.get());
+
+    dropStuff(con.get());
 
 
-    commit(con);
-    delete con;
+    commit(con.get());
 
     if(assertionsFailed>0) {
-      cout << assertionsFailed << " assertions failed." << endl;
+      ODBCXX_COUT << assertionsFailed
+                  << ODBCXX_STRING_CONST(" assertions failed.") << endl;
     }
   } catch(SQLException& e) {
-    cerr << endl << e.getMessage() << endl;
+    ODBCXX_CERR << endl << e.getMessage() << endl;
     return 1;
   }
 
