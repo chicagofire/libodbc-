4a971391943a407952b55386ee701f173c6ff3fc
lothrop <lothrop@5ab6fc7a-e072-45e7-acf8-72c2705bde4f>
2005-09-07 14:23:33 +0000

    Made tests work with wide strings.

--- trunk/libodbc++/tests/dmtest.cpp
+++ trunk/libodbc++/tests/dmtest.cpp
@@ -1,18 +1,18 @@
-/* 
+/*
    This file is part of libodbc++.
-   
+
    Copyright (C) 1999-2000 Manush Dodunekov <manush@stendahls.net>
-   
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
-   
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
-   
+
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.  If not, write to
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
@@ -22,12 +22,14 @@
 #include <odbc++/drivermanager.h>
 
 #include <iostream>
+#include <memory>
 
 using namespace odbc;
 using namespace std;
 
 #if defined(ODBCXX_QT)
-ostream& operator<<(ostream& o, const QString& s)
+basic_ostream<ODBCXX_CHAR_TYPE>& operator<<(basic_ostream<ODBCXX_CHAR_TYPE>& o,
+                                            const QString& s)
 {
   o << ODBCXX_STRING_CSTR(s);
   return o;
@@ -37,45 +39,41 @@ ostream& operator<<(ostream& o, const QString& s)
 int main(void)
 {
   try {
-    cout << "Available datasources: " << endl;
+    ODBCXX_COUT << ODBCXX_STRING_CONST("Available datasources: ") << endl;
 
-    DataSourceList* l=DriverManager::getDataSources();
+    std::auto_ptr<DataSourceList> l=
+      std::auto_ptr<DataSourceList>(DriverManager::getDataSources());
 
     for(DataSourceList::iterator i=l->begin();
-	i!=l->end(); i++) {
+        i!=l->end(); i++) {
       DataSource* ds=(*i);
-      cout << "\t" << ds->getName();
-      
+      ODBCXX_COUT << ODBCXX_STRING_CONST("\t") << ds->getName();
+
       if(ds->getDescription().length()>0) {
-	cout << " (" << ds->getDescription() << ")";
+        ODBCXX_COUT << ODBCXX_STRING_CONST(" (") << ds->getDescription()
+                    << ODBCXX_STRING_CONST(")");
       }
 
-      cout << endl;
+      ODBCXX_COUT << endl;
     }
 
-    delete l;
+    ODBCXX_COUT << ODBCXX_STRING_CONST("Available drivers: ") << endl;
 
-    cout << "Available drivers: " << endl;
-    
-
-    DriverList* dl=DriverManager::getDrivers();
+    std::auto_ptr<DriverList> dl=std::auto_ptr<DriverList>(DriverManager::getDrivers());
 
     for(DriverList::iterator j=dl->begin();
-	j!=dl->end(); j++) {
-      cout << "\t" << (*j)->getDescription() << endl;
-      
+        j!=dl->end(); j++) {
+      ODBCXX_COUT << ODBCXX_STRING_CONST("\t") << (*j)->getDescription() << endl;
+
       const vector<ODBCXX_STRING>& attrs=(*j)->getAttributes();
       for(vector<ODBCXX_STRING>::const_iterator x=attrs.begin(); x!=attrs.end(); x++) {
-	cout << "\t\tAttribute: " << *x << endl;
+        ODBCXX_COUT << ODBCXX_STRING_CONST("\t\tAttribute: ") << *x << endl;
       }
 
-      cout << endl;
+      ODBCXX_COUT << endl;
     }
-    
-    delete dl;
-
   } catch(SQLException& e) {
-    cerr << endl << e.getMessage() << endl;
+    ODBCXX_CERR << endl << e.getMessage() << endl;
   }
 
   return 0;
